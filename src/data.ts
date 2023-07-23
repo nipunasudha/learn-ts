4b8c56a59fcff9be69f5cbe0b0382ffa4ac6a357
ParagonTheDev <nipuna.sudha@gmail.com>
2023-07-23 21:00:09 +0530

    feat: change names

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 import {Student} from "./student";
 import {Teacher} from "./teacher";
 
-export const myStudent1 = new Student("John", 15)
-export const myTeacher1 = new Teacher("Alice", 40)
+export const myStudent1 = new Student("Kamal", 15)
+export const myTeacher1 = new Teacher("Nimal", 40)
